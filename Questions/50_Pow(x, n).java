76a4d8b3c3ff4aeca6a4fb99928f185373cc4e99
shalr-20 <shalini.roy2035@gmail.com>
2024-09-05 13:36:22 +0530

    Pow(x, n)

--- Questions/50_Pow(x, n).java
+++ Questions/50_Pow(x, n).java
@@ -2,7 +2,14 @@ class Solution {
     public double myPow(double x, int n) {
         if (n == 0) {
             return 1;
-        } else if (n < 0) {
+        }
+        if (n < 0) {
+            if (n == Integer.MIN_VALUE) {
+                x = 1 / x;
+                n = Integer.MAX_VALUE;
+                return x * myPow(x, n);
+            }
+
             x = 1 / x;
             n = -n;
         }
@@ -16,6 +23,7 @@ class Solution {
             x *= x;
             n /= 2;
         }
+
         return res;
     }
 }
