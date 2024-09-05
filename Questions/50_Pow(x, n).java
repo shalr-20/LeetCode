1a314b6fedcb452d97577e8156e8ebdb13a91e67
shalr-20 <shalini.roy2035@gmail.com>
2024-09-05 13:32:19 +0530

    Pow(x, n)

---
+++ Questions/50_Pow(x, n).java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        } else if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+
+        double res = 1;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                res *= x;
+            }
+
+            x *= x;
+            n /= 2;
+        }
+        return res;
+    }
+}
