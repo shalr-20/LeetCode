ce739592605354cd58b45ac31889cb70299e22da
shalr-20 <shalini.roy2035@gmail.com>
2024-09-10 23:12:30 +0530

    First_Unique_Character_in_a_String

---
+++ Questions/387_First_Unique_Character_in_a_String.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (charCount.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
